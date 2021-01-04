e7e0b78b52d8d35961a70f15af4bfbe330204617
Shree3245 <shreevenkr@gmail.com>
2021-01-04 13:53:59 +0530

    added upload size limit

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const filesRouter = require("./routes/files");
 const app = express();
 var io = require("socket.io")(server);
 
+//Allow for transactions to be up to 50MB
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+
 // DB Setup
 mongoose
   .connect(process.env.DB_CONECTION, {
